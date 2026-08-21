efcd7dc8e45b5ad19fa39899015ec2e6b4ce90dc
Foxkid <alserkol90@gmail.com>
2026-08-22 00:12:13 +0400

    Preconditions and postconditions for automated testing has been added.

---
+++ src/test/java/ru/netology/selenium/CallbackMyVTest.java
@@ -0,0 +1,35 @@
+package ru.netology.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CallbackMyVTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+}
