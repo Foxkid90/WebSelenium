2f93f86cb4de810bb6e26e6d77a8fd8dd084ba72
Foxkid <alserkol90@gmail.com>
2026-08-23 02:43:06 +0400

    An autotest has been developed for a positive scenario.

--- src/test/java/ru/netology/selenium/CallbackMyVTest.java
+++ src/test/java/ru/netology/selenium/CallbackMyVTest.java
@@ -3,9 +3,15 @@ package ru.netology.selenium;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CallbackMyVTest {
 
@@ -29,6 +35,20 @@ public class CallbackMyVTest {
     void tearDown() {
         driver.quit();
         driver = null;
+    }
+    @Test
+    void shouldSubmitTheApplicationSuccessfully() {
+        WebElement form = driver.findElement(By.cssSelector("form[enctype=\"application/x-www-form-urlencoded\"]"));
+        form.findElement(By.cssSelector("span[data-test-id=name] input[value]")).sendKeys("Врангель Петр");
+        form.findElement(By.cssSelector("span[data-test-id=phone] input[value]")).sendKeys("+79170001922");
+        form.findElement(By.cssSelector("label[data-test-id=agreement] span[class=checkbox__box]")).click();
+        form.findElement(By.cssSelector("button[type=button]")).click();
+        // Из за наличия задержки возникновения отчётного сообщения, использование cssSelector приводит к падению теста.
+        // В связи с этим принято решение использовать className: "paragraph".
+        //WebElement message = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        WebElement message = driver.findElement(By.className("paragraph"));
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", message.getText().trim());
+
     }
 
 
